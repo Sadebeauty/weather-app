6dce9e5eb76a41906bb0082d8b61c6ec2b41af7e
Sadebeauty <124639292+Sadebeauty@users.noreply.github.com>
2023-04-20 23:35:58 +0100

    created city search function

--- script.js
+++ script.js
@@ -33,16 +33,27 @@ function displayWeather(response) {
   windSpeed.innerHTML = Math.round(response.data.wind.speed);
   descriptionElement = document.querySelector("#description");
   descriptionElement.innerHTML = response.data.condition.description;
-  cloudLogo = document.querySelector(".cloud-logo");
+  cloudLogo = document.querySelector("#icon");
   cloudLogo.innerHTML = response.data.condition.icon;
   dateElement = document.querySelector("#date");
   dateElement.innerHTML = formatDate(response.data.time * 1000);
   iconElement = document.querySelector("#icon");
   iconElement.setAttribute = response.data.condition.icon;
 }
-let apiKey = "83f2f0t26352d3o664bcf8a01bce4fa7";
-let defaultCity = "Lagos";
-let apiUrl =
-  "https://api.shecodes.io/weather/v1/current?query=ibadan&key=83f2f0t26352d3o664bcf8a01bce4fa7";
+function search(city) {
+  let apiKey = "83f2f0t26352d3o664bcf8a01bce4fa7";
+  let apiUrl =
+    "https://api.shecodes.io/weather/v1/current?query=${city}&key=83f2f0t26352d3o664bcf8a01bce4fa7";
+  axios.get(apiUrl).then(displayWeather);
+}
 
-axios.get(apiUrl).then(displayWeather);
+function citySubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-form");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#city-search");
+form.addEventListener("submit", search);
+
+search("Ibadan");
